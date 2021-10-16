be80438002b1ead1a43e2d442c9426e2448fad31
polinaidu007 <polinaidusigilipalli@gmail.com>
2021-10-17 01:58:59 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.min.css"
+import firebase from "firebase/app"
+import "firebase/auth"
+import Home from "./pages/Home"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import PageNotFound from './pages/PageNotFound';
+import { UserContext } from './context/UserContext';
+
+
 
 function App() {
+  const [user, setUser] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="*" component={PageNotFound} />
+        </Switch>
+
+      </UserContext.Provider>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/pages/Signin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Signin() {
+    return (
+        <div>
+            <h1> Sign in page</h1>
+        </div>
+    );
+}
+
+export default Signin;
