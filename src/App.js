257b283bfcbd6c588cff8c660d4a1ffb6b537ade
polinaidu007 <polinaidusigilipalli@gmail.com>
2021-10-17 18:36:58 +0530

    Added footer and conditional rendering in header

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import PageNotFound from './pages/PageNotFound';
 import { UserContext } from './context/UserContext';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
 
 
 
@@ -21,12 +23,14 @@ function App() {
     <Router>
       <ToastContainer />
       <UserContext.Provider value={{ user, setUser }}>
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/signin" component={Signin} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="*" component={PageNotFound} />
         </Switch>
+        <Footer />
 
       </UserContext.Provider>
     </Router>
---
+++ src/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+function Footer() {
+    return (
+        <Container fluid tag="footer" className="text-center bg-info text-hite text-uppercase fixed-bottom p-3">
+            Github search app
+        </Container>
+    );
+}
+
+export default Footer;
